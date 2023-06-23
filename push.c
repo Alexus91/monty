4ab4008f253be1501323804316b99666ea653bec
Alexus91 <mrfadlkamal@gmail.com>
2023-06-22 19:16:03 -0700

    fixing push

--- push.c
+++ push.c
@@ -8,7 +8,8 @@
  */
 void push(stack_t **stack, int value)
 {
-	stack_t *new_node = malloc(sizeof(stack_t));
+	stack_t *new_node = NULL;
+	new_node = malloc(sizeof(stack_t));
 
 	if (new_node == NULL)
 	{
@@ -20,15 +21,14 @@ void push(stack_t **stack, int value)
 	new_node->n = value;
 	new_node->prev = NULL;
 
-	if (*stack == NULL)
-	{
-		new_node->next = NULL;
-	}
-	else
+	if (*stack != NULL)
 	{
 		new_node->next = *stack;
 		(*stack)->prev = new_node;
 	}
-
+	else
+	{
+		new_node->next = NULL; 
+	}
 	*stack = new_node;
 }
